ff5c48f89d8a350d6b0c394e017a73cff73ef755
Wellington Issami Yoshida <issami-yoshida@hotmail.com>
2018-08-28 19:24:32 -0300

    Criação da tela de login

--- src/main/java/br/com/sistemaAtividade/securiity/WebSecurityConfig.java
+++ src/main/java/br/com/sistemaAtividade/securiity/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package br.com.sistemaAtividade.securiity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +24,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .formLogin()
                     .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
                     .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .inMemoryAuthentication()
+                .withUser("yoshida").password("$2a$10$dC2W9Eb83mt16JSUNUx.vuOAUMgS..cl1LtKnTmPnpHOCHx1Bo3UO").roles("EDITOR", "ADMIN")
+                .and()
+                .withUser("fernanda").password("$2a$10$dC2W9Eb83mt16JSUNUx.vuOAUMgS..cl1LtKnTmPnpHOCHx1Bo3UO").roles("EDITOR");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
